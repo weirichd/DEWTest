76d88f64329c2bca52e20a866fa20c70e9eb71c5
David E. Weirich <weirich.david@gmail.com>
2015-08-10 20:51:30 -0600

    Printing Source when a test fails

--- dewtest.c
+++ dewtest.c
@@ -1,10 +1,17 @@
 #include "dewtest.h"
 
+int mul(int a, int b)
+{
+	return a * b;
+}
+
 int main()
 {
 	DEW_assert_true(1);
-	DEW_assert_true(0);
+	DEW_assert_true(0); // This should fail
 	DEW_assert_false(0);
+
+	DEW_assert_true(mul(3,4) == 12);
 
 	return 0;
 }
--- dewtest.h
+++ dewtest.h
@@ -19,40 +19,70 @@
  */
 
 /*
- * DEWTest is a very small testing suite for C.
+ * DEWTest is a very small testing library for C.  It is only required that this header file
+ * be included.
  */
 
-#ifndef __DEWTEST__INCLUDED__
-#define __DEWTEST__INCLUDED__
+#ifndef __DEWTEST__INCLUDED
+#define __DEWTEST__INCLUDED
 
 #include <stdio.h>
 
-/*********************
+/*******************
  *  Color Defines
- *********************/
+ *******************/
 
 #define __DEWTEST__RED(text) "\e[41m"#text"\e[0m"
 #define __DEWTEST__GREEN(text) "\e[42m"#text"\e[0m"
 
+/************************
+ *  Source Code Output
+ ************************/
 
+#define __DEWTEST__READ__SUCCESS 1
+#define __DEWTEST__READ__FAIL 0
 
-static inline void __DEWTEST__ASSERT__TEST__(int thing_we_are_testing,
-                                             char *file,
-                                             int line)
+static inline int __DEWTEST__PRINT__SOURCE__LINE(char *filename, int line)
+{
+	FILE *fh = fopen(filename, "r");
+	if(!fh)
+	{
+		return __DEWTEST__READ__FAIL;	
+	}	
+
+	char buffer[256];
+
+	for(int current_line = 0; current_line < line; current_line++)
+	{
+		fgets(buffer, 256, fh);
+	}
+
+	fclose(fh);
+	
+	printf("%s", buffer);
+
+	return __DEWTEST__READ__SUCCESS;
+}
+
+/*******************************
+ *  Internal Assert Functions
+ *******************************/
+
+#define DEW_assert_true(true_thing) __DEWTEST__ASSERT__TRUE(true_thing, __FILE__, __LINE__)
+#define DEW_assert_false(false_thing) __DEWTEST__ASSERT__TRUE(!false_thing, __FILE__, __LINE__)
+static inline void __DEWTEST__ASSERT__TRUE(int thing_to_be_tested, char *file, int line)
 {
 	printf("FILE: %s  LINE: %d\t", file, line);
 
-	if(thing_we_are_testing)
+	if(thing_to_be_tested)
 	{
 		printf(__DEWTEST__GREEN(Test Passed)"\n");
 	}
 	else
 	{
 		printf(__DEWTEST__RED(Test Failed)"\n");
+		__DEWTEST__PRINT__SOURCE__LINE(file, line);
 	}
 }
 
-#define DEW_assert_true(true_thing) __DEWTEST__ASSERT__TEST__(true_thing, __FILE__, __LINE__)
-#define DEW_assert_false(false_thing) __DEWTEST__ASSERT__TEST__(!false_thing, __FILE__, __LINE__)
-
-#endif  /* __DEWTEST__INCLUDED__ */
+#endif  /* __DEWTEST__INCLUDED */
